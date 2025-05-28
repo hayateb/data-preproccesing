8531bda860ff61e2ecc1da517c6e90aece5d2345
Hayat Ebrahim <ebrahimhayat241@gmail.com>
2025-05-28 13:23:32 +0300

    iris dataset cleaning

--- iris/iris.py
+++ iris/iris.py
@@ -1,10 +1,19 @@
+# use the standard scaler to scale the iris dataset
+# and catagory the species column with numerical values
 import pandas as pd
 import numpy as np
 import seaborn as sns
-from  sklearn.preprocessing import LabelEncoder
+from  sklearn.preprocessing import LabelEncoder ,StandardScaler
 
 df = sns.load_dataset('iris', cache = True , data_home = None )
 
-df_clean = pd.get_dummies(df, columns= ['species'])
 
-print(df_clean)
+encoder = LabelEncoder()
+
+df['species'] = encoder.fit_transform(df['species'])
+cols = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+
+for col in cols:
+      scale = StandardScaler()
+      df[col] = scale.fit_transform(df[[col]])
+print(df)

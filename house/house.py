41d1aba36e524f4e8a5bb61e900d8de4f6980d94
Hayat Ebrahim <ebrahimhayat241@gmail.com>
2025-05-28 11:57:03 +0300

    house datacleaning

--- house/house.py
+++ house/house.py
@@ -1,13 +1,14 @@
+# the preproccesing i used for this cleaning are catagorical encoding using banary encoding
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder 
+
 
 df = pd.read_csv('Housing.csv')
-
 df_house = pd.DataFrame(df)
 
-df_ecoded = pd.get_dummies(df_house , columns=['furnishingstatus'])
+for col in ['mainroad', 'prefarea', 'furnishingstatus']:
+    encoder = LabelEncoder()  # or your encoder
+    df_house[col] = encoder.fit_transform(df_house[col])
 
-
-
-print(df_ecoded)
+print(df_house)
